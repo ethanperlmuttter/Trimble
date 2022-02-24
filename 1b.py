12f3ca5f169e5f83cb49b8ea5f1e2732efcc1848
eperl <eperl@umich.edu>
2022-02-24 17:11:05 -0500

    q3 and q4

--- 1b.py
+++ 1b.py
@@ -7,7 +7,7 @@ d = {'StudentID': ['V001', 'V002', 'V003', 'V004'] , 'Name': ['Abe', 'Abhay', 'A
 name_table = pd.DataFrame(data= d)
 
 #returns a new data frame version of name_table, where each name
-containing the letter “e” is uppercased, and lowercased otherwise
+#containing the letter “e” is uppercased, and lowercased otherwise
 def to_upper_or_lower (table = name_table):
     t = table.copy(deep = True)
     t['Name']  = t['Name'].apply(lambda x : x.upper() if 'e' in x.lower() else x.lower())
